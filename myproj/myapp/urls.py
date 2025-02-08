c78a530b3b336b55287acd60e6a9689d733d1ab4
Ait <keniusmals@gmail.com>
2025-02-08 22:53:16 +0300

    More work on templates and view functions

--- myproj/myapp/urls.py
+++ myproj/myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     path('hello', hello),
+    path('about', about, name='about'),
+    path('anime-list', anime_list, name='anime-list'),
 ]
--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, f@#ing world!")
+    return HttpResponse('Hello, f@#ing world!')
 
 def index(request):
-    return render(request, "myapp/index.html")
+    return render(request, 'myapp/index.html', {'index_active': True})
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def anime_list(request):
+    return render(request, 'myapp/anime-list.html', {'anime_list_active': True})
