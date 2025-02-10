fc57f8cb3f7583080cfab9d5f8d99c2cbe36fb62
Ait <keniusmals@gmail.com>
2025-02-10 21:55:13 +0300

    Correction in table in index.html Add anime model!

--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Anime(models.Model):
+    name = models.CharField(max_length=90)
+    additional_name = models.CharField(max_length=250)
+    short_description = models.TextField()
+    large_description = models.TextField()
+    photo = models.ImageField(upload_to ='anime-img/%Y/%m/%d/')
+    main_num_rating = models.DecimalField(max_digits=10, decimal_places=2)
+    my_top_rating = models.IntegerField()
+
