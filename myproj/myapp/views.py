1ff7f928697448c34aa01be76850c4bde01dd302
Ait <keniusmals@gmail.com>
2025-02-15 23:05:54 +0300

    Add filter in anime-list

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -22,9 +22,27 @@ def about(request, pk):
     return render(request, 'myapp/about.html', context=context)
 
 def anime_list(request):
-    anime_list = Anime.objects.all()
+    filter = int(request.GET.get('filter', 0))
+
+    filter_dict = {
+        1: 'Объективный топ',
+        2: 'Личный топ',
+        0: 'По алфавиту',
+    }
+
+    if filter == 1:
+        anime_list = Anime.objects.order_by('-main_num_rating')
+    elif filter == 2:
+        anime_list = Anime.objects.order_by('my_top_rating')
+    else:
+        anime_list = Anime.objects.order_by('name')
+
+    filter_label = filter_dict[filter]
+
     context = {
         'anime_list_active': True,
         'anime_list': anime_list,
+        'filter_label': filter_label,
     }
+
     return render(request, 'myapp/anime-list.html', context=context)
