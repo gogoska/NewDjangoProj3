f741a12d8e10731671d4d6dee565390b899f1159
Ait <keniusmals@gmail.com>
2025-03-11 11:32:34 +0300

    Make beautiful in admin panel

--- myproj/myapp/admin.py
+++ myproj/myapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Anime
 
-admin.site.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'main_num_rating', 'my_top_rating')
+	list_display_links = ('name',)
+	search_fields = ('name',)
+	list_editable = ('main_num_rating', 'my_top_rating')
+	list_filter = ('name', 'main_num_rating', 'my_top_rating')
 
-# Register your models here.
+
+admin.site.register(Anime, AnimeAdmin)
--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -7,11 +7,16 @@ import os
 # Create your models here.
 
 class Anime(models.Model):
-    name = models.CharField(max_length=90)
-    additional_name = models.CharField(max_length=250)
-    short_description = models.TextField()
-    large_description = models.TextField()
-    photo = models.ImageField(upload_to='anime-img/%Y/%m/%d/')
-    main_num_rating = models.DecimalField(max_digits=10, decimal_places=2)
-    my_top_rating = models.IntegerField()
+    name = models.CharField(max_length=90, verbose_name='Название')
+    additional_name = models.CharField(max_length=250, verbose_name='Доп. название', blank=True)
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
+    large_description = models.TextField(verbose_name='Длинное описание', blank=True)
+    photo = models.ImageField(upload_to='anime-img/%Y/%m/%d/', verbose_name='Картика', blank=True)
+    main_num_rating = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Главный рейтинг')
+    my_top_rating = models.IntegerField(verbose_name='Мой рейтинг')
+
+    class Meta:
+        verbose_name = 'Аниме'
+        verbose_name_plural = 'Анимешки'
+        ordering = ['name']
 
